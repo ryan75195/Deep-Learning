23af655fe9bd28807c99fae9236776da5e8a6f70
gizsak <78755277+gizsak@users.noreply.github.com>
2022-03-05 17:00:16 +0100

    Update experimentBuilder.py
    uses weight decay as well

--- MLPcluster/experimentBuilder.py
+++ MLPcluster/experimentBuilder.py
@@ -1,11 +1,4 @@
 import train_model
 
 #resnet18
-train_model.run_experiment(18,1e-3)
-train_model.run_experiment(18,1e-4)
-train_model.run_experiment(18,1e-5)
-
-#resnet34
-train_model.run_experiment(34,1e-3)
-train_model.run_experiment(34,1e-4)
-train_model.run_experiment(34,1e-5)
+train_model.run_experiment(18,1e-3,0.1)
